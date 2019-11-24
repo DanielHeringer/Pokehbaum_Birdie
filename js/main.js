376c7fcadc856a967a3b52d3c3b458df05922e62
Daniel Heringer <danielheringer2012@gmail.com>
2019-11-24 09:42:24 -0300

    ultimo commit antes da refatoração para VueJS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var url = window.location.href;   
 index = url.indexOf('?'); 
-if(index > -1){
-    document.getElementById('transition-background').style.transform="translateX(0%)";
+console.log(url.substring(index+1, index+3));
+
+if(index > -1 && url.substring(index+1, index+3) == "id"){
+    document.getElementById('stats').style.transform="translateX(0%)";
 }
 
 function transitionIn(id){
-    document.getElementById('transition-background').style.transform="translateX(0%)";
+    document.getElementById('stats').style.transform="translateX(0%)";
     setTimeout(function(){
         var url = window.location.href;   
         index = url.indexOf('?'); 
@@ -19,11 +21,18 @@ function transitionIn(id){
     }, 200);
 }
 function transitionOut(){
-    document.getElementById('transition-background').style.transform="translateX(100%)";
+    document.getElementById('stats').style.transform="translateX(100%)";
     setTimeout(function(){
         var url = window.location.href;   
         index = url.indexOf('?'); 
         url = url.substring(0, index);
         window.location.href = url;
     }, 200);
+}
+
+function getPokemonList(s){
+    
+}
+function getStatsPokemon(id){
+
 }
